f43d4ed8dcc96fb43de15edec29a51ac40a01da3
Pablo Montesdeoca Pérez <pmontesd@gmail.com>
2020-06-11 11:05:41 +0100

    add min size to detector

--- viola-jones.py
+++ viola-jones.py
@@ -5,7 +5,7 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-def detect_faces(cascade, image, scaleFactor=1.1):
+def detect_faces(cascade, image, scaleFactor=1.1, minSize=(30, 30)):
     # Converting to grayscale
     image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # Face detection
@@ -24,7 +24,7 @@ haar_cascade_frontalface = cv2.CascadeClassifier('haarcascade_frontalface_defaul
 p = Path('Database_real_and_fake_face_160x160')
 
 with open('rectangles.csv', 'w', newline='') as csvfile:
-    fieldnames = ['x', 'y', 'w', 'h']
+    fieldnames = ['filename', 'x', 'y', 'w', 'h']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
 
@@ -35,10 +35,10 @@ with open('rectangles.csv', 'w', newline='') as csvfile:
         image = cv2.imread(str(imageFile))
         
         # Face detection
-        faces_rects = detect_faces(haar_cascade_frontalface, image)
+        faces_rects = detect_faces(haar_cascade_frontalface, image, scaleFactor=1.1, minSize=(160*0.5, 160*0.5))
         if (len(faces_rects)):
             (x, y, w, h) = faces_rects[0]
-            writer.writerow({'x': x, 'y': y, 'w': w, 'h': h})
+            writer.writerow({'filename': imageFile, 'x': x, 'y': y, 'w': w, 'h': h})
         
         # Let's print the no. of faces fond
         print('Faces found: ', len(faces_rects))
